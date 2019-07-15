799f78d3aa3cdf9ae633eb945e4a5230288e6bfe
ahmadsalimi <ahsalimi@ce.sharif.edu>
2019-07-15 11:32:57 +0430

    initial commit
    start asp.net

---
+++ PlasticSearch/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlasticSearch.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string Search(string query)
+        {
+            return "not found";
+        }
+
+        [HttpPost]
+        public int Preprocess()
+        {
+            Thread.Sleep(3000);
+            return 3000;
+        }
+    }
+}
